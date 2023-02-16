cba1f57512cf92e79e66f0094b242138cbadb635
Vaibhav Chauhan <chauhanvaibhav9602@gmail.com>
2023-02-16 15:53:55 +0530

    added Flipmove

--- src/Feed.js
+++ src/Feed.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import './Feed.css'
-import TweetBox from './TweetBox'
-import Post from './Post'
-import db from './firebase'
+import React, { useEffect, useState } from 'react';
+import './Feed.css';
+import TweetBox from './TweetBox';
+import Post from './Post';
+import db from './firebase';
+import FlipMove from 'react-flip-move';
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -24,25 +25,28 @@ function Feed() {
         <TweetBox />
 
         {/* Post */}
-
-        {posts.map(post => (
-          <Post
-          displayName={post.displayName}
-          username={post.username}
-          verified={post.verified}
-          text={post.text}
-          avatar={post.avatar}
-          image={post.image}
-          />
-        ))}
+        <FlipMove>
+          {posts.map(post => (
+            <Post
+              key={post.text}
+              displayName={post.displayName}
+              username={post.username}
+              verified={post.verified}
+              text={post.text}
+              avatar={post.avatar}
+              image={post.image}
+            />
+          ))}
+        </FlipMove>
 
         {/* <Post 
-        displayName="Vaibhav Chauhan" 
-        username="Vaibhav_962" 
-        verified={true} 
-        text="Yeah its running perfectly" 
-        avatar="/profile_picture.jpg" 
-        image="https://media.tenor.com/P8VsDwHZreYAAAAC/tobey-maguire-spider-man.gif" /> */}
+          displayName="Vaibhav Chauhan" 
+          username="Vaibhav_962" 
+          verified={true} 
+          text="Yeah its running perfectly" 
+          avatar="/profile_picture.jpg" 
+          image="https://media.tenor.com/P8VsDwHZreYAAAAC/tobey-maguire-spider-man.gif" 
+        /> */}
     </div>
   )
 }
--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,5 @@
 import { Avatar } from '@mui/material';
-import React from 'react';
+import React, { forwardRef } from 'react';
 import './Post.css'
 import VerifiedIcon from '@mui/icons-material/Verified';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
@@ -7,9 +7,16 @@ import RepeatIcon from '@mui/icons-material/Repeat';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import PublishIcon from '@mui/icons-material/Publish';
 
-function Post({ displayName, username, verified, text, image, avatar }) {
+const Post = forwardRef(({ 
+    displayName, 
+    username, 
+    verified, 
+    text, 
+    image, 
+    avatar 
+}, ref) => {
   return (
-    <div className='post'>
+    <div className='post' ref={ref}>
          <div className="post__avatar">
             <Avatar src={avatar}/>
          </div>
@@ -37,6 +44,6 @@ function Post({ displayName, username, verified, text, image, avatar }) {
          </div>
     </div>
   )
-}
+});
 
 export default Post
